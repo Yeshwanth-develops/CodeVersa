04f4a58878e1c29b68383b78587957403ffb172b
Yeshwanth Sunkara <154045970+Yeshwanth-develops@users.noreply.github.com>
2026-02-17 14:56:52 +0000

    Prime or not

---
+++ Prime.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        boolean isPrime = true;
+
+        if (n <= 1 ) {
+            isPrime = false;
+        }
+
+        for (int i=2;i*i <= n;i++) {
+            if (n % i == 0) {
+                isPrime = true;
+                break;
+            }
+        }
+        if (isPrime) {
+            System.out.println("Prime number");
+        } else {
+            System.out.println("Not a prime number");
+        }
+    }
+}
